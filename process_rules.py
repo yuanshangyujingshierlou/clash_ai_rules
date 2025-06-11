2e4019ca19c52f69ec0f452d60bb4d4f6ee57dc2
qiyonghan <qiyonghan10998@xwtec.cn>
2025-06-11 11:45:53 +0800

    文件分类

--- process_rules.py
+++ process_rules.py
@@ -1,5 +1,6 @@
 import os
 import re
+import yaml
 
 raw_data = os.environ.get("RAW_DATA")
 if not raw_data:
@@ -8,6 +9,7 @@ if not raw_data:
 
 domains = set()
 ips = set()
+process_names = set()
 
 for line in raw_data.splitlines():
     line = line.strip()
@@ -23,11 +25,21 @@ for line in raw_data.splitlines():
         parts = line.split(",")
         if len(parts) > 1:
             ips.add(parts[1].strip())
+    elif line.startswith("PROCESS-NAME,"):
+        process_names.add(line.split(",")[1].strip())
 
 with open("domains.list", "w") as f:
     for domain in sorted(list(domains)):
         f.write(domain + "\n")
 
+ip_list = sorted(list(ips))
+yaml_data_ips = {"payload": ip_list}
+
 with open("ips.list", "w") as f:
-    for ip in sorted(list(ips)):
-        f.write(ip + "\n")
+    yaml.dump(yaml_data_ips, f, sort_keys=False)
+
+process_name_list = sorted(list(process_names))
+yaml_data_process_names = {"payload": process_name_list}
+
+with open("process_names.list", "w") as f:
+    yaml.dump(yaml_data_process_names, f, sort_keys=False)
